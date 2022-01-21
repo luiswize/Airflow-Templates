27cf291c5a0d65e9942328a51c1a8c8dd9202455
luiswize <97264380+luiswize@users.noreply.github.com>
2022-01-21 17:28:48 -0600

    Update connection_dag.py

--- connection_dag.py
+++ connection_dag.py
@@ -1,39 +1,39 @@
-import pandas as pd
 from airflow import DAG
 from datetime import datetime
 # from airflow.operators.postgres_operator import PostgresOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.cloud_sql import CloudSQLImportInstanceOperator
 
 def yeah():
     print('yeah this is cool')
-    
 # raw_data = pd.read_csv('https://storage.googleapis.com/resources_data_eng_app04/user_purchase.csv')
-with DAG(
-    dag_id="postgres_operator_dag",
-    start_date=datetime(2020, 2, 2),
-    schedule_interval="@once",
-    catchup=False,
-) as dag:
-    create_user_purchase_table = PostgresOperator(
-        task_id = 'create_user_purchase_table',
-        sql = """
-        CREATE SCHEMA Milestone;
-        CREATE TABLE Milestone.user_purchase(
-            invoice_number varchar(10),
-            stock_code varchar(20),
-            detail varchar(1000),
-            quantity int,
-            invoide_date timestamp,
-            unit_price numeric(8,3),
-            customer_id int,
-            country varchar(20)
-        ); 
-        """
+
+
+import_body = {"importContext": {
+    "fileType": "csv",
+    "uri": 'gs://resources_data_eng_app04/user_purchase.csv',
+    "database": 'dbname',
+    "csvImportOptions": {
+    "table": 'apprenticeship.user_purchase',
+        },
+    "importUser": 'dbuser'
+    }}
+
+with DAG(dag_id="postgres_operator_dag", start_date=datetime(2021, 1, 1),
+    schedule_interval="@once", catchup=False) as dag:
+    sql_import_task = CloudSQLImportInstanceOperator(
+        task_id='sql_import_task',
+        project_id='gcp-data-eng-appr04-cee96a91',
+        body = import_body,
+        instance='sql-milestone-5',
+        gcp_conn_id='my_gcp_connection'
     )
+
     t2 = PythonOperator(
         task_id='yeah',
         python_callable=yeah
     )
-    
-    create_user_purchase_table >> t2
+
+    sql_import_task >> t2
+
