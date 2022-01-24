2ff92412e974880c4dc240067c46c4b7202a65c4
luiswize <97264380+luiswize@users.noreply.github.com>
2022-01-24 13:16:13 -0600

    Update connection_dag.py

--- connection_dag.py
+++ connection_dag.py
@@ -1,3 +1,4 @@
+
 from airflow import DAG
 from datetime import datetime
 # from airflow.operators.postgres_operator import PostgresOperator
@@ -5,6 +6,7 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.google.cloud.operators.cloud_sql import CloudSqlInstanceImportOperator
 
+#CloudSqlInstanceImportOperator
 def yeah():
     print('yeah this is cool')
 # raw_data = pd.read_csv('https://storage.googleapis.com/resources_data_eng_app04/user_purchase.csv')
@@ -22,6 +24,7 @@ import_body = {"importContext": {
 
 with DAG(dag_id="postgres_operator_dag", start_date=datetime(2021, 1, 1),
     schedule_interval="@once", catchup=False) as dag:
+    
     sql_import_task = CloudSqlInstanceImportOperator(
         task_id='sql_import_task',
         project_id='gcp-data-eng-appr04-cee96a91',
@@ -36,5 +39,3 @@ with DAG(dag_id="postgres_operator_dag", start_date=datetime(2021, 1, 1),
     )
 
     sql_import_task >> t2
-
-
