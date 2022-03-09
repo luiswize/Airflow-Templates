2238e5cf08ee98b680ea216112e6ba3ccb9d72b8
luiswize <97264380+luiswize@users.noreply.github.com>
2022-03-09 14:25:27 -0600

    Update and rename spark_movies_dag.py to capstone_project_dag.py

--- capstone_project_dag.py
+++ capstone_project_dag.py
@@ -1,14 +1,25 @@
 from airflow import DAG
-from datetime import datetime 
+from airflow.hooks.postgres_hook import PostgresHook
+from datetime import timedelta
+from datetime import datetime
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.models import BaseOperator
+from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFilesystemOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.google.cloud.operators.dataproc import DataprocCreateClusterOperator \
                         ,DataprocDeleteClusterOperator \
                         ,DataprocSubmitJobOperator
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 
-
+def csv_to_postgres():
+    pg_hook = PostgresHook(postgres_conn_id='postgres_default')
+    get_postgres_conn = pg_hook.get_conn()
+    cur = get_postgres_conn.cursor()
+    with open("user_purchase.csv", "r") as f:
+        cur.copy_expert("COPY user_purchase FROM STDIN WITH CSV HEADER", f)
+        get_postgres_conn.commit()
+    cur.close()
 
 CLUSTER_CONFIG = {
     "master_config": {
@@ -35,11 +46,54 @@ PYSPARK_LOGS_JOB = {
     "pyspark_job": {"main_python_file_uri": 'gs://codes-gcp-data-eng-appr04-cee96a91/Spark_log_reviews.py'}
 }
 
+default_args = {
+    'owner': 'luis.morales',
+    'depends_on_past': False,
+    'start_date': datetime(2021, 1, 21),
+    'email': ['luis.morales@wizeline.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
 with DAG("spark_jobs", 
     start_date=datetime(2021, 1 ,1), 
     schedule_interval='@once', 
+    default_args=default_args,
     catchup=False  # Catchup  
     ) as dag:
+    
+    create_postgres = PostgresOperator(task_id='create_postgres_table',
+                         sql="""
+                        CREATE SCHEMA staging;
+                        CREATE TABLE IF NOT EXISTS staging.user_purchase (    
+                            invoice_number varchar(10),
+                            stock_code varchar(20),
+                            detail varchar(1000),
+                            quantity int,
+                            invoice_date timestamp,
+                            unit_price numeric(8,3),
+                            customer_id int,
+                            country varchar(20)
+                        );
+                            """,
+                         postgres_conn_id='postgres_default',
+                         autocommit=True,
+            )
+
+    download_gcs_file = GCSToLocalFilesystemOperator(task_id="download_gcs_file",
+                                     bucket="data-apprenticeship",
+                                     object_name="user_purchase.csv",
+                                     filename="user_purchase.csv",
+                                     gcp_conn_id="google_cloud_default",
+                                    )
+
+    csv_to_database = PythonOperator(task_id='csv_to_database',
+                       provide_context=True,
+                       python_callable=csv_to_postgres,
+                       )
+
     #-----------------CREATES CLUSTERS IN DATAPROC------------------
 
     create_movies_cluster = DataprocCreateClusterOperator(
@@ -87,7 +141,8 @@ with DAG("spark_jobs",
         project_id='gcp-data-eng-appr04-cee96a91', 
         region = 'us-west1', 
         cluster_name='movies-review' ,
-        gcp_conn_id = 'google_cloud_default'
+        gcp_conn_id = 'google_cloud_default',
+        trigger_rule='all_done'
     )
     
     delete_logs_cluster = DataprocDeleteClusterOperator(
@@ -95,6 +150,8 @@ with DAG("spark_jobs",
         project_id='gcp-data-eng-appr04-cee96a91', 
         region = 'us-west1', 
         cluster_name='logs-review' ,
-        gcp_conn_id = 'google_cloud_default'
+        gcp_conn_id = 'google_cloud_default',
+        trigger_rule='all_done'
     )
-    create_movies_cluster >> create_log_cluster >> [pyspark_movies_task, pyspark_logs_task] >> delete_movies_cluster >> delete_logs_cluster
+    create_postgres >> download_gcs_file >> csv_to_database >> create_movies_cluster >> create_log_cluster >> [pyspark_movies_task, pyspark_logs_task]\
+             >> delete_movies_cluster >> delete_logs_cluster
