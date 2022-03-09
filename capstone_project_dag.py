cd63c991d69c169973786f0fd3790ad9e297397d
luiswize <97264380+luiswize@users.noreply.github.com>
2022-03-09 17:58:40 -0600

    Update capstone_project_dag.py

--- capstone_project_dag.py
+++ capstone_project_dag.py
@@ -66,7 +66,7 @@ with DAG("spark_jobs",
     
     create_postgres = PostgresOperator(task_id='create_postgres_table',
                          sql="""
-                        CREATE SCHEMA staging;
+                        CREATE SCHEMA staging2;
                         CREATE TABLE IF NOT EXISTS staging.user_purchase (    
                             InvoiceNo varchar(10),
                             StockCode varchar(20),
